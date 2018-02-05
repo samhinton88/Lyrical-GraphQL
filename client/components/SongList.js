3f6e37c6f8bf03661733fa90d5ea424190039ca9
sam hinton <sam.hinton88@gmail.com>
2018-02-05 17:15:33 +0000

    added logic to implement mutation in onClick even handler l52

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,13 +5,23 @@ import { Link } from 'react-router';
 import query from '../queries/fetchSongs'
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id }, refetchQueries: [ { query } ] })
+  }
+
   renderSongs() {
     if (this.props.data.loading) { return <div>Loading...</div> }
 
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          { title }
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete
+          </i>
         </li>
       )
     })
