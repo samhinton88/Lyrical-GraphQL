6070762a3fe657ceb704960a930e07bc3fa9c9c9
sam hinton <sam.hinton88@gmail.com>
2018-02-05 15:04:36 +0000

    imported data into SongList component, added workaround for undefined data l40

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    if (this.props.data.loading) { return <div>Loading...</div> }
 
+    return this.props.data.songs.map(song => {
+      return (
+        <li>
+          {song.title}
+        </li>
+      )
+    })
+  }
 
   render() {
     return (
-      <div>SongList</div>
+      <div>
+        {this.renderSongs()}
+      </div>
     )
   }
 }
@@ -19,4 +32,4 @@ const query = gql`
   }
 `;
 
-export default SongList
+export default graphql(query)(SongList);
