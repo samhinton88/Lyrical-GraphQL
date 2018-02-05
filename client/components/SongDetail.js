098f496a50d872dc29f353d16c9f371580f99ff5
sam hinton <sam.hinton88@gmail.com>
2018-02-05 19:00:28 +0000

    added option to graphql integration to allow query to run with params argument l57

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
 
 
-
   render() {
+    console.log(this.props)
     return (
       <div>
         <h3>Song Detail</h3>
@@ -14,6 +15,7 @@ class SongDetail extends Component {
   }
 }
 
-
-
-export default SongDetail
+// provide variables to the query before it is made
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
